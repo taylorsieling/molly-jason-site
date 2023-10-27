08e4ee194ae62dc363a45ceab965fd5c8f28dffc
taylorsieling <taylorasieling@gmail.com>
2023-10-27 15:10:39 -0500

    setup vue-router

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Itinerary from "../views/Itinerary.vue";
+import OurStory from "../views/OurStory.vue";
+import Registry from "../views/Registry.vue";
+import Rsvp from "../views/Rsvp.vue";
+import TravelInfo from "../views/TravelInfo.vue";
+
+const routes = [
+  { path: "/", name: "Home", component: Home },
+  { path: "/our-story", name: "Our Story", component: OurStory },
+  { path: "/itinerary", name: "Itinerary", component: Itinerary },
+  { path: "/rsvp", name: "RSVP", component: Rsvp },
+  { path: "/travel-info", name: "Travel Info", component: TravelInfo },
+  { path: "/registry", name: "Registry", component: Registry },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+export default router;
