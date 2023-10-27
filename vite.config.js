7b3fd23567164808d241037ca2f757f73d534449
taylorsieling <taylorasieling@gmail.com>
2023-10-27 14:27:49 -0500

    initial setup

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import inkline from '@inkline/plugin/vite';
-
-const inklineConfig = {
-    outputDir: 'src/css/variables'
-};
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [inkline(inklineConfig), vue()]
-});
+  plugins: [vue()]
+})
