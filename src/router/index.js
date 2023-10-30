0deee32b8883f1778e1b3f161dc0829bbf5b3fc2
taylorsieling <taylorasieling@gmail.com>
2023-10-29 22:17:03 -0500

    change itinerary to schedule

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
-import Itinerary from "../views/Itinerary.vue";
+import Schedule from "../views/Schedule.vue";
 import OurStory from "../views/OurStory.vue";
 import Registry from "../views/Registry.vue";
 import Rsvp from "../views/Rsvp.vue";
@@ -9,7 +9,7 @@ import TravelInfo from "../views/TravelInfo.vue";
 const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/our-story", name: "Our Story", component: OurStory },
-  { path: "/itinerary", name: "Itinerary", component: Itinerary },
+  { path: "/schedule", name: "schedule", component: Schedule },
   { path: "/rsvp", name: "RSVP", component: Rsvp },
   { path: "/travel-info", name: "Travel Info", component: TravelInfo },
   { path: "/registry", name: "Registry", component: Registry },
