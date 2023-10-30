8884f53da333942c0c32593094148252bf87969f
taylorsieling <taylorasieling@gmail.com>
2023-10-29 22:14:50 -0500

    setup daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,20 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
-  }
+  daisyui: {
+    themes: [{
+        mytheme: {
+          "primary": "#8c9cac",
+          "secondary": "#d0b08b",
+          "accent": "#36484a",
+          "neutral": "#1A1F20",
+          "base-100": "#f6f6f9",
+          "info": "#b2bec8",
+          "success": "#808b6c",
+          "warning": "#ca8a04",
+          "error": "#ac8c9c",
+        },
+      }],
+  },
+  plugins: [require("daisyui"),]
+}
