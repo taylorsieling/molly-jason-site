a18df985b4085341a9b93edbe99b357d97e4e63a
taylorsieling <taylorasieling@gmail.com>
2023-11-03 23:35:34 -0500

    build two column components with text + image

---
+++ src/components/ImageLeftSection.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="flex flex-wrap md:flex-nowrap justify-start md:justify-around">
+    <div class="basis-1/3 p-2 md:p-6">
+      <img id="side-image" class="sharp-left" :src=imgSrc>
+    </div>
+    <div class="basis-2/3 p-2 md:p-6 mt-4 md:mt-0">
+      <div class="min-w-3 mt-8">
+        <h2 class="text-sm uppercase tracking-wide mb-4">
+          <slot name="supertitle"></slot>
+        </h2>
+        <h3 class="cormorant text-6xl mb-4">
+          <slot name="title"></slot>
+        </h3>
+        <p class="text-md leading-relaxed mb-4">
+          <slot name="content"></slot>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      imgSrc: String
+    }
+  }
+</script>
+
+<style>
+#side-image {
+  min-width: 365px;
+  height: 432px;
+  object-fit: cover;
+}
+
+.sharp-left {
+  border-top-right-radius: 5rem;
+  border-bottom-left-radius: 5rem;
+  border-bottom-right-radius: 5rem;
+}
+</style>
---
+++ src/components/ImageRightSection.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex flex-wrap-reverse md:flex-nowrap justify-end md:justify-around text-end">
+    <div class="basis-2/3 p-2 md:px-8 mt-4 md:mt-0">
+      <div class="min-w-3 mt-8">
+        <h2 class="text-sm uppercase tracking-wide mb-4">
+          <slot name="supertitle"></slot>
+        </h2>
+        <h3 class="cormorant text-6xl mb-4">
+          <slot name="title"></slot>
+        </h3>
+        <p class="text-md leading-relaxed mb-4">
+          <slot name="content"></slot>
+        </p>
+      </div>
+    </div>
+    <div class="basis-1/3 p-2 md:px-8">
+      <img id="side-image" class="sharp-right" :src=imgSrc>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: { 
+      imgSrc: String
+    },
+  }
+</script>
+
+<style>
+.sharp-right {
+  border-top-left-radius: 5rem;
+  border-bottom-left-radius: 5rem;
+  border-bottom-right-radius: 5rem;
+}
+</style>
