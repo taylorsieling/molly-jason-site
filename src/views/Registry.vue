c0ebe55ec433e238b5f123bda23bb4d9b63ceec5
taylorsieling <taylorasieling@gmail.com>
2023-11-03 23:51:41 -0500

    initial setup: registry + rsvp pages

--- src/views/Registry.vue
+++ src/views/Registry.vue
@@ -1,9 +1,20 @@
 <template>
-  <div></div>
+  <PageTitle :titleImage=titleImage overlayClass="bg-zinc-700/40">
+    <template #title>Registry</template>
+  </PageTitle>
 </template>
 
 <script>
-export default {};
+import PageTitle from '../components/PageTitle.vue';
+
+export default {
+  data() {
+    return {
+      titleImage: 'background-image: url(/images/mj-registry.jpg)'
+    }
+  },
+  components: { PageTitle }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style></style>
--- src/views/Rsvp.vue
+++ src/views/Rsvp.vue
@@ -1,9 +1,20 @@
 <template>
-  <div></div>
+  <PageTitle :titleImage=titleImage overlayClass="bg-slate-600/30">
+    <template #title>RSVP</template>
+  </PageTitle>
 </template>
 
 <script>
-export default {};
+import PageTitle from '../components/PageTitle.vue';
+
+export default {
+  data() {
+    return {
+      titleImage: 'background-image: url(/images/mj-rsvp.jpg)'
+    }
+  },
+  components: { PageTitle }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style></style>
