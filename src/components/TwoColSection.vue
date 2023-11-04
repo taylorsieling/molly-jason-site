50151f07e15fedee2258efd499e399671cc9fed7
taylorsieling <taylorasieling@gmail.com>
2023-11-03 23:31:40 -0500

    build home page + two column component

---
+++ src/components/TwoColSection.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex flex-wrap md:flex-nowrap justify-start md:justify-around mx-auto my-12 w-10/12">
+    <div class="basis-1/2 p-4 md:p-6 text-center bg-primary text-white">
+      <p class="mb-5 uppercase playfair tracking-wider">
+        <slot name="supertitle" ></slot>
+      </p>
+      <p class="mb-10 text-6xl script italic">
+        <slot name="title"></slot>
+      </p>
+      <p class="mb-5 text-3xl uppercase tracking-widest font-light playfair">
+        <slot name="subtitle"></slot>
+      </p>
+      <p class="mb-5 text-xl">
+        <slot name="content"></slot>
+      </p>
+    </div>
+    <div class="hero basis-1/2">
+      <img :src=rightImage class="object-cover">
+      <div class="hero-overlay bg-slate-600/20"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      rightImage: String
+    }
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,84 @@
 <template>
-  <section>
-    <!-- Hero Image -->
-    <div id="hero w-100 m-4">
-      <img src="/images/molly-jason-achandler-316.jpg">
-    </div>
-    <!-- Invitation -->
-    <div id="welcome" class="text-center">
-      <div class="mt-10">
-        <p class="mb-5 italic">You are cordially invited to the wedding of</p>
-        <h1 class="mb-10 text-6xl script italic">Molly Gosky & Jason Young</h1>
-        <h2 class="mb-5 text-3xl uppercase">April 26, 2025</h2>
-        <h2 class="mb-5 text-xl">Charleston, South Carolina</h2>
+  <section id="hero" class="h-screen">
+    <div class="hero h-full bg-top bg-cover" style="background-image: url(/images/mj-home.jpg);">
+      <div class="hero-overlay bg-slate-700/50"></div>
+      <div class="hero-content text-center text-white">
+        <div>
+          <div class="mb-12">
+            <p class="text-2xl uppercase tracking-widest mb-6">Save the Date</p>        
+            <h1 class="text-8xl playfair uppercase tracking-wider mb-6">Molly & Jason</h1>
+            <p class="text-xl uppercase tracking-widest mb-12">are tying the knot</p>
+            <p class="text-6xl cormorant italic tracking-wider">April 26, 2025</p>  
+          </div>
+        </div>
       </div>
     </div>
-    <!-- Countdown -->
-    <div class="text-center text-4xl">
-      <h1>JUST THE TWO OF US</h1>
+  </section>
+  <section id="countdown">
+    <div id="cd-container" class="container w-9/12 text-stone-500 mx-auto my-8 p-12">
+      <p class="text-xl text-center uppercase tracking-widest mb-12">Let the countdown begin!</p>        
       <vue-countdown :time="time" v-slot="{ days, hours, minutes, seconds }">
-        {{ days }} days, {{ hours }} hours, {{ minutes }} minutes
+        <div class="flex justify-between text-center">
+          <div :class=cdClass>
+            <span class="text-9xl leading-3 cormorant italic mb-20">{{ days }}</span>
+            <span class="text-base tracking-widest">days</span>
+          </div>
+          <div :class=cdClass>
+            <span class="text-9xl leading-3 cormorant italic mb-20">{{ hours }}</span>
+            <span class="text-sm tracking-wide">hours</span>
+          </div>
+          <div :class=cdClass>
+            <span class="text-9xl leading-3 cormorant italic mb-20">{{ minutes }}</span>
+            <span class="text-sm tracking-wide">minutes</span>
+          </div>
+          <div :class=cdClass>
+            <span class="text-9xl leading-3 cormorant italic mb-20">{{ seconds }}</span>
+            <span class="text-sm tracking-wide">seconds</span>
+          </div>
+        </div>
       </vue-countdown>
+    </div>
+  </section>
+  <section>
+    <TwoColSection :rightImage=rightImage>
+      <template #supertitle>You are cordially invited to the wedding of</template>
+      <template #title>Molly Gosky & <br> Jason Young</template>
+      <template #subtitle>April 26, 2025</template>
+      <template #content>Charleston, South Carolina</template>
+    </TwoColSection>
+  </section>
+  <section id="invitation">
+    <div class="text-center p-12">
+      <div class="bg-white w-2/3 py-16 px-12 mx-auto">
+        <p class="mb-5 uppercase playfair tracking-wider">You are cordially invited to the wedding of</p>
+        <h1 class="mb-10 text-6xl script italic">Molly Gosky & Jason Young</h1>
+        <h2 class="mb-5 text-3xl uppercase tracking-widest font-light playfair">April 26, 2025</h2>
+        <h2 class="mb-5 text-xl">Charleston, South Carolina</h2>
+      </div>
     </div>
   </section>
 </template>
 
-<script>
+<script lang="ts">
 import VueCountdown from '@chenfengyuan/vue-countdown';
+import TwoColSection from '../components/TwoColSection.vue'
 
 export default {
-  components: { VueCountdown },
+  components: { VueCountdown, TwoColSection },
   data() {
     const now = new Date();
     const weddingDay = new Date('April 26, 2025 18:00:00');
-    console.log(weddingDay)
 
     return {
+      btnClass: 'cormorant italic tracking-widest border py-2 px-6 hover:scale-110 transition duration-300 ease-in-out',
+      cdClass: 'flex flex-col basis-1/4 justify-center uppercase p-2',
+      rightImage: '/images/mj-beachlaugh.jpg',
       time: weddingDay - now,
     };
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+
+</style>
