de6d328bcbd5648c6c47a32cb1d2177678b2bafc
taylorsieling <taylorasieling@gmail.com>
2023-11-03 23:49:49 -0500

    schedule page

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -1,9 +1,24 @@
 <template>
-  <div></div>
+  <div class="flex flex-wrap md:flex-nowrap justify-between items-start static">
+    <div class="h-full hero lg:basis-1/3">
+      <img class="h-72 md:min-h-screen w-full object-top object-cover self-auto" src="/images/mj-greenery.jpeg">
+      <div class="hero-overlay bg-zinc-700/40"></div>
+    </div>
+    <div class="lg:basis-2/3 p-16">
+      <Timeline />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import Timeline from '../components/TimeLine.vue'
+
+export default {
+  components: { Timeline }
+
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+
+</style>
