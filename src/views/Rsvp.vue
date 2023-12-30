a2c93b6a4a375c1ddd9aa9d26618592ea236aac8
taylorsieling <taylorasieling@gmail.com>
2023-12-29 19:02:10 -0600

    rsvp page content

--- src/views/Rsvp.vue
+++ src/views/Rsvp.vue
@@ -2,18 +2,24 @@
   <PageTitle :titleImage=titleImage overlayClass="bg-slate-600/30">
     <template #title>RSVP</template>
   </PageTitle>
+  <CenteredTextSection>
+    <template #supertitle>We hope you can join us on our special day.</template>
+    <template #title>Invitations are coming soon!</template>
+    <template #content>Official invitations will be sent out in 2024. Once you receive your invitation, you can revisit this page to send in your RSVP.</template>
+  </CenteredTextSection>
 </template>
 
 <script>
 import PageTitle from '../components/PageTitle.vue';
+import CenteredTextSection from '../components/CenteredTextSection.vue';
 
 export default {
+  components: { CenteredTextSection, PageTitle },
   data() {
     return {
       titleImage: 'background-image: url(/images/mj-rsvp.jpg)'
     }
-  },
-  components: { PageTitle }
+  }
 };
 </script>
 
