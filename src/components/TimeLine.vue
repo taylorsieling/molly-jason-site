bd2a604b897adbfffea06bfda97a4feba72f6033
taylorsieling <taylorasieling@gmail.com>
2024-08-12 18:08:17 -0500

    Updates to Timeline content

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="-my-6">
     <div class="relative pl-8 sm:pl-32 py-6 group mb-2">
-      <div class="text-sm italic mb-1 sm:mb-0">TBA</div>
+      <div class="text-sm italic mb-1 sm:mb-0">5:30 PM</div>
       <div :class="timeline">
         <time class="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-white bg-primary rounded-full">April 25</time>
-        <div class="text-xl text-stone-900 playfair uppercase tracking-wider">Happy Hour</div>
+        <div class="text-xl text-stone-900 playfair uppercase tracking-wider">Party at the Point: Welcome Party
+        </div>
       </div>
       <div class="text-stone-500">
-        <span class="font-bold">Charleston Harbor Resort & Marina | Bridge Bar </span><br>
-        Welcome to Charleston! Say hello to the bride and groom before the big day at this casual Happy Hour! 
+        <span class="font-bold">Charleston Harbor Resort & Marina | Beachfront </span><br>
+        Come hang out with us at a welcome party on the beachfront! Party at the Point is hosted by the Charleston Harbor Resort and features live music and plenty of food and drinks.        <span class="italic">Tickets will be provided and you can purchase drinks and food at the event.</span>
       </div>
     </div>
     <div class="relative pl-8 sm:pl-32 py-6 group mb-2">
-      <div class="text-sm italic mb-1 sm:mb-0">4:00 PM</div>
+      <div class="text-sm italic mb-1 sm:mb-0">4:30 PM - 5:00 PM</div>
       <div :class="timeline">
         <time class="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-white bg-primary rounded-full">April 26</time>
         <div class="text-xl text-stone-900 playfair uppercase tracking-wider">Ceremony</div>
@@ -23,14 +24,25 @@
       </div>
     </div>
     <div class="relative pl-8 sm:pl-32 py-6 group">
-      <div class="text-sm italic mb-1 sm:mb-0">6:00 PM</div>
+      <div class="text-sm italic mb-1 sm:mb-0">5:00 PM - 6:00 PM</div>
       <div :class="timeline">
         <time class="sm:absolute left-0 translate-y-0.5 md:hidden inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-white bg-primary rounded-full">April 26</time>
-        <div class="text-xl text-stone-900 playfair uppercase tracking-wider">Reception</div>
+        <div class="text-xl text-stone-900 playfair uppercase tracking-wider">Cocktail Hour</div>
       </div>
       <div class="text-stone-500">
         <span class="font-bold">Charleston Harbor Resort & Marina | Sunrise Terrace & Deck</span><br>
-        The knot has been tied... let's celebrate! We will have a buffet dinner, open bar, and lots of dancing. 
+        Enjoy an open bar while dinner is being prepared!
+      </div>
+    </div>
+    <div class="relative pl-8 sm:pl-32 py-6 group">
+      <div class="text-sm italic mb-1 sm:mb-0">6:00 PM - 10:00 PM</div>
+      <div :class="timeline">
+        <time class="sm:absolute left-0 translate-y-0.5 md:hidden inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-white bg-primary rounded-full">April 26</time>
+        <div class="text-xl text-stone-900 playfair uppercase tracking-wider">Dinner & Dancing</div>
+      </div>
+      <div class="text-stone-500">
+        <span class="font-bold">Charleston Harbor Resort & Marina | Sunrise Terrace & Deck</span><br>
+        The knot has been tied... let's celebrate! We will have a buffet dinner, open bar, and lots of dancing!
       </div>
     </div>
   </div>
