f3b37a44222d6ffa45f78b7fa616760bc9e2d590
taylorsieling <taylorasieling@gmail.com>
2024-10-21 12:22:05 -0500

    add hotel information

--- src/views/TravelInfo.vue
+++ src/views/TravelInfo.vue
@@ -2,15 +2,22 @@
   <PageTitle :titleImage=titleImage overlayClass="bg-zinc-700/40">
     <template #title>Travel & Accomodations</template>
   </PageTitle>
-  <CenteredTextSection class="p-4 md:py-4 md:px-48">
+  <CenteredTextSection class="p-4 md:py-4 md:px-48" data-aos="fade-in">
     <template #supertitle>We are so excited to have all of our favorite people in one place!</template>
     <template #title>Charleston, South Carolina</template>
     <template #content>
       As we get closer to the big day, we will share plenty of information to make your travel as easy as possible, including lodging recommendations, transportation information, and the best ways to get around Charleston. We will even put together a guide to our favorite Charleston restaurants, hangouts, and activities. 
+      <div class="mt-2">
+        <ul class="flex text-center align-items-center">
+          <li><a href="#venue">The Venue</a></li>
+          <li><a>Lodging</a></li>
+          <li><a>Item 3</a></li>
+        </ul>
+      </div>
     </template>
   </CenteredTextSection>
   <hr>
-  <div class="p-4 md:py-4 md:px-48">
+  <div id="venue" class="p-4 md:py-4 md:px-48">
     <ImageLeftSection :imgSrc=venueImage>
       <template #supertitle>The Venue</template>
       <template #title>Charleston Harbor Resort & Marina</template>
@@ -28,24 +35,44 @@
       </template>
     </ImageRightSection>
   </div>
-  
-  <!-- <div class="bg-primary text-white text-center p-8 md:py-4 md:px-48">
-    <h3 class="text-4xl cormorant uppercase tracking-widest p-4">Lodging</h3>
-    <p class="text-md leading-relaxed mb-4">We have room blocks at the following hotels - just mention the "Gosky/Young Wedding" when making your reservations.</p>
-    <div class="flex flex-col lg:flex-row justify-center text-center text-zinc-800">
-      <div class="lg:basis-1/3 m-2 p-8 bg-white sharp-right">
-        <p></p>
+  <div id="lodging" class="bg-primary text-white text-center p-8 md:py-4 md:px-24">
+    <h3 class="text-5xl cormorant uppercase tracking-wide p-4" data-aos="fade-in">Lodging</h3 >
+    <p class="text-md leading-relaxed mb-4">Charleston is a popular location! We want our guests to have fun and book a trip they will love. Therefore, we will not have a room blocks at any hotels to allow guests as much flexibility as possible.</p>
+    <p class="text-md leading-relaxed mb-4">See our hotel recommendations below for nearby options in a variety of prices ranges.</p>
+    <p class="">* * *</p>
+    <div class="flex flex-col lg:flex-row justify-center text-center">
+      <div class="lg:basis-1/3 m-2 p-2">
+        <img class="rounded-xl mb-3 h-40" src="https://cache.marriott.com/content/dam/marriott-renditions/CHSMT/chsmt-exterior-0001-hor-wide.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1336px:*">
+        <h4 class="text-4xl cormorant mb-3">SpringHill Suites by Marriot</h4>
+        <p class="text-sm mb-2">
+          245 Magrath Darby Blvd <br>
+          Mt Pleasant, SC 29464
+        </p>
+        <p class="italic text-sm mb-4">Price Range: $</p>
+        <a href="https://www.marriott.com/en-us/hotels/chsmt-springhill-suites-charleston-mount-pleasant/overview/?scid=f2ae0541-1279-4f24-b197-a979c79310b0" class="px-5 py-3 bg-white text-primary italic text-bold hover:bg-primary hover:text-white hover:border-2">Reservations</a>
       </div>
-      <div class="lg:basis-1/3 m-2 p-8 bg-white sharp-right"></div>
-      <div class="lg:basis-1/3 m-2 p-8 bg-white sharp-right">
-        <h4>Charleston Harbor Resort & Marina</h4>
-        <p>
+      <div class="lg:basis-1/3 m-2 mt-12 md:mt-2 p-2">
+        <img class="rounded-xl mb-3 h-40 w-full object-cover" src="https://www.hilton.com/im/en/CHSSCES/15644848/es-chssces-night-exterior.jpg?impolicy=crop&cw=5000&ch=3255&gravity=NorthWest&xposition=0&yposition=40&rw=768&rh=500">
+        <h4 class="text-4xl cormorant mb-3">Embassy Suites by Hilton</h4>
+        <p class="text-sm mb-2">
+          100 Ferry Wharf Road <br>
+          Mt Pleasant, SC 29464
+        </p>
+        <p class="italic text-sm mb-4">Price Range: $</p>
+        <a href="https://www.hilton.com/en/hotels/chssces-embassy-suites-charleston-harbor-mt-pleasant/rooms/" class="px-5 py-3 bg-white text-primary italic text-bold hover:bg-primary hover:text-white hover:border-2">Reservations</a>
+      </div>
+      <div class="lg:basis-1/3 m-2 mt-12 md:mt-2 p-2">
+        <img class="rounded-xl mb-3 h-40 w-full object-cover" src="https://www.charlestonharborresort.com/files/3792/img-beachclub-002.jpg">
+        <h4 class="text-4xl cormorant mb-3">Charleston Harbor Resort & Marina</h4>
+        <p class="text-sm mb-2">
           20 Patriots Point Rd <br>
           Mt Pleasant, SC 29464
         </p>
+        <p class="italic text-sm mb-4">Price Range: $$</p>
+        <a href="https://bookings.travelclick.com/110419?adobe_mc=MCMID=10089339946301935712685362941743018248|MCORGID=1C1238B352785AA60A490D4C%40AdobeOrg|TS=1723506574&domain=www.charlestonharborresort.com#/guestsandrooms" class="px-5 py-3 bg-white text-primary italic text-bold hover:bg-primary hover:text-white hover:border-2">Reservations</a>
       </div>
     </div>
-  </div> -->
+  </div>
 </template>
 
 <script>
